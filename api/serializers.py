9170c31d2edda414f5e5f41687090b4db1afbaf5
Saveliy <miheev.saveliy.2002@gmail.com>
2023-05-04 22:41:22 +0800

    Fix

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model, authenticate
 
-
 from api.models import *
 
 UserModel = get_user_model()
@@ -10,11 +9,13 @@ UserModel = get_user_model()
 class BotDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Bot
-        fields = '__all__' # явно указываем все поля модели
+        fields = '__all__'  # явно указываем все поля модели
+
+
 class BotsListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Bot
-        fields = ('app_name', 'token', 'url', 'name', 'launch_status', 'login_id')
+        fields = ("id",'app_name', 'token', 'url', 'name', 'launch_status', 'login_id')
 
 
 class CommandDetailSerializer(serializers.ModelSerializer):
@@ -25,8 +26,8 @@ class CommandDetailSerializer(serializers.ModelSerializer):
 
 class CommandsListView(serializers.ModelSerializer):
     class Meta:
-        model = Bot
-        fields = ('bot_id', 'command_name', 'type_id', 'link_status', 'media_status')
+        model = Command
+        fields = ("id","command_name","link_status","media_status","bot_id_id","type_id_id")
 
 
 class CommandLinkDetailSerializer(serializers.ModelSerializer):
@@ -35,7 +36,19 @@ class CommandLinkDetailSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-class CommandsLinkListView(serializers.ModelSerializer):
+class CommandLinksListView(serializers.ModelSerializer):
     class Meta:
         model = Link_command
-        fields = ('current_command', 'following_command')
+        fields = "__all__"
+
+
+class CommandTypeDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Type_command
+        fields = "__all__"
+
+
+class CommandTypesListView(serializers.ModelSerializer):
+    class Meta:
+        model = Type_command
+        fields = "__all__"
